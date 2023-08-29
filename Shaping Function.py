cab7817b9c533d09aa71c87fbe65af9b0b12a6ca
Kaviyashree J <112486595+JKaviyashree@users.noreply.github.com>
2023-08-29 12:26:04 +0530

    Update Shaping Function.py

--- Shaping Function.py
+++ Shaping Function.py
@@ -4,7 +4,6 @@ import cv2
 img = np.zeros((400,400,3),dtype="uint8")
 cv2.rectangle(img,(30,30),(300,200),(0,255,0),5)
 cv2.imshow("Rectangle",img)
-cv2.waitKey()
 
 #Square
 import numpy as np
@@ -12,7 +11,6 @@ import cv2
 img = np.zeros((400,400,3),dtype="uint8")
 cv2.rectangle(img,(30,30),(300,300),(0,255,0),5)
 cv2.imshow("Square",img)
-cv2.waitKey()
 
 #Line
 import numpy as np
@@ -20,7 +18,6 @@ import cv2
 img = np.zeros((400,400,3),dtype="uint8")
 cv2.line(img,(20,160),(100,160),(0,255,0),5)
 cv2.imshow("Line",img)
-cv2.waitKey()
 
 #Circle
 import numpy as np
@@ -28,4 +25,3 @@ import cv2
 img = np.zeros((400,400,3),dtype="uint8")
 cv2.circle(img,(200,200),80,(0,255,0),5)
 cv2.imshow("Circle",img)
-cv2.waitKey()
