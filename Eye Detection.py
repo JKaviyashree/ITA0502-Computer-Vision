4fdade5e40a94a300f97820f2fc304f237a31b8d
Kaviyashree J <112486595+JKaviyashree@users.noreply.github.com>
2023-08-31 09:21:17 +0530

    Update Eye Detection.py

--- Eye Detection.py
+++ Eye Detection.py
@@ -1,10 +1,26 @@
 import cv2
-img = cv2.imread(r"C:\Computer Vision\makne.jpg")
-gray_img = cv2.cvtColor(img,cv2.IMREAD_GRAYSCALE)
-haarcascade =cv2.CascadeClassifier(cv2.data.haarcascades+"haarcascade_eye.xml")
-faces_rec = haarcascade.detectMultiScale(gray_img,1.1,10)
-for (x,y,w,h) in faces_rec:
-    cv2.rectangle(img,(x,y),(x+w,y+h), (0,255,0), 2)
 
-cv2.imshow('Detected Eyes',img)
+# Load the pre-trained eye detection model (Haar Cascade Classifier)
+eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
+
+# Load the image
+image_path = r"c:/Users/student/Desktop/Computer Vision/jk-v-jimin.jfif"
+img = cv2.imread(image_path)
+#img=cv2.resize(img,(400,400))
+# Convert the image to grayscale for eye detection
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# Detect eye in the grayscale image
+eye = eye_cascade.detectMultiScale(gray,1.1,10)
+
+# Print the number of eye detected
+print(f"Number of Eye detected: {len(eye)}")
+
+# Draw rectangles around the detected eye
+for (x, y, w, h) in eye:
+    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+# Display the image with rectangles around the eye
+cv2.imshow('Eye Detection', img)
 cv2.waitKey(0)
+cv2.destroyAllWindows()
